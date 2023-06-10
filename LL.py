97daa2dd91b7afce21d4449baf4381b529ed4023
Andy Santana <andysantana@Andys-MacBook-Pro.local>
2023-06-10 00:13:48 -0500

    first commit

---
+++ LL.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def add_list_elements(self, elements):
+        for element in elements:
+            self.add_node(element)
+
